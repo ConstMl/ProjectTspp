988b00f5d8dbc99775db3d0fe128b0fa937d88bd
Константин <k-malinov@bk.ru>
2019-05-09 00:10:01 +0300

    Дописана реализация методов класса CustomerList, добавлен форматированный вывод

--- ProjectTspp/CustomerList.cs
+++ ProjectTspp/CustomerList.cs
@@ -20,39 +20,87 @@ namespace ProjectTspp
 
         public void View()
         {
+            Console.WriteLine($"------------------------------------------------------------");
+            Console.WriteLine($"|Имя            |Фамилия        |Паспорт     |Телефон      |");
+            Console.WriteLine($"------------------------------------------------------------");
             foreach (var cust in customers)
             {
-                Console.WriteLine($"|{cust.Name}\t |{cust.Surname}\t |{cust.PasportSeriesNumber}\t |{cust.Phone}\t |");
+                Console.WriteLine($"|{cust.Name,-15}|{cust.Surname,-15}|{cust.PasportSeriesNumber,12}|{cust.Phone,13}|");
             }
+            Console.WriteLine($"------------------------------------------------------------");
+        }
+
+        private void ViewItem(Customer cust)
+        {
+            Console.WriteLine($"------------------------------------------------------------");
+            Console.WriteLine($"|Имя            |Фамилия        |Паспорт     |Телефон      |");
+            Console.WriteLine($"------------------------------------------------------------");
+            Console.WriteLine($"|{cust.Name,-15}|{cust.Surname,-15}|{cust.PasportSeriesNumber,12}|{cust.Phone,13}|");
+            Console.WriteLine($"------------------------------------------------------------");
         }
 
         public void Add()
         {
             var item = new Customer();
-            Console.Write("Имя: ");
-            item.Name = Console.ReadLine();
-            Console.Write("Фамилия: ");
-            item.Surname = Console.ReadLine();
-            Console.Write("Номер паспорта: ");
-            long temp;
-            while (!Int64.TryParse(Console.ReadLine(), out temp))
+            long tempLong;
+            string tempString;
+
+            Console.Write("Имя: "); tempString = Console.ReadLine();
+            while (tempString.Length > 12)
+            {
+                Console.Write("Данные введены неверно (длинное имя), повторите ввод: ");
+                tempString = Console.ReadLine();
+            }
+            item.Name = tempString;
+
+            Console.Write("Фамилия: "); tempString = Console.ReadLine();
+            while (tempString.Length > 12)
+            {
+                Console.Write("Данные введены неверно (длинная фамилия), повторите ввод: ");
+                tempString = Console.ReadLine();
+            }
+            item.Surname = tempString;
+
+            Console.Write("Серия и номер паспорта: ");
+            while (!Int64.TryParse(Console.ReadLine(), out tempLong))
             {
                 Console.Write("Данные введены неверно, повторите ввод: ");
             }
-            item.PasportSeriesNumber = temp;
+            item.PasportSeriesNumber = tempLong;
+
             Console.Write("Номер телефона: ");
-            while (!Int64.TryParse(Console.ReadLine(), out temp))
+            while (!Int64.TryParse(Console.ReadLine(), out tempLong))
             {
                 Console.Write("Данные введены неверно, повторите ввод: ");
             }
-            item.Phone = temp;
+            item.Phone = tempLong;
+
             customers.Add(item);
             Console.WriteLine($"Клиен добавлен {customers.Count}");
         }
 
         public void Search()
         {
-            throw new NotImplementedException();
+            long pasportSeriesNumber;
+            Console.Write("Серия и номер паспорта");
+            while (!Int64.TryParse(Console.ReadLine(), out pasportSeriesNumber))
+            {
+                Console.Write("Данные введены неверно, повторите ввод: ");
+            }
+            bool searchFlag = false;
+            foreach (var cust in customers)
+            {
+                if (cust.PasportSeriesNumber == pasportSeriesNumber)
+                {
+                    ViewItem(cust);
+                    searchFlag = true;
+                    break;
+                }
+            }
+            if (!searchFlag)
+            {
+                Console.WriteLine("Такого клиента не существует.");
+            }
         }
     }
 }
